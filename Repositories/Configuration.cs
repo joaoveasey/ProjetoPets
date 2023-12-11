265a7d3083d10bf4aff65cef277478094f11b3b8
joaoveasey <jovepedro@gmail.com>
2023-12-10 22:55:47 -0300

    commit

---
+++ Repositories/Configuration.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Repositories
+{
+    public static class Configuration
+    {
+        public static string getConnectionString()
+        {
+            return "Server=LAPTOP-JLE3IV2Q;Database=pets; Trusted_Connection=True; TrustServerCertificate=True;";
+        }
+    }
+}
